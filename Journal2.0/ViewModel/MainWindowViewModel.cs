9f8e363dae036868c4f6106f87acf76e84eceb6b
@DanekBB777 <bekenev.daniilkpc582020@gmail.com>
2023-03-29 10:35:22 +0300

    First

---
+++ Journal2.0/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Journal2._0.ViewModel.Base;
+
+namespace Journal2._0.ViewModels
+{
+    internal class MainWindowViewModel: BaseViewModel
+    {
+        protected string _title;
+
+        public string Title 
+        { 
+            get => _title; 
+            set => Set(ref _title, value);
+        }
+    }
+}
